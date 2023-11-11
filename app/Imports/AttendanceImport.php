53af5c5c46e66cfaa960735ede61ceea920fa153
Danish <softwaredeveloper167@gmail.com>
2023-11-12 02:33:47 +0500

    Skip those records which is not valid in excel uploading file

--- app/Imports/AttendanceImport.php
+++ app/Imports/AttendanceImport.php
@@ -16,22 +16,26 @@ use Maatwebsite\Excel\Concerns\OnEachRow;
 class AttendanceImport implements OnEachRow, WithHeadingRow
 {
     public function onRow(Row $row)
-    {   
+    {
         $location = Location::select('id')->where('name', $row['location'])->first();
         $shift = Shift::select('id')->where('title', $row['shift'])->first();
         $employee = Employee::select('id')->where('email', $row['email'])->first();
 
-        $schedule = Schedule::firstOrCreate([
-            'shift_id' => $shift->id,
-            'location_id' => $location->id,
-            'attendance_date' => Date::excelToDateTimeObject($row['attendance_date']) ,
-        ]);
+        if ($location->count() > 0 && $shift->count() > 0 && $employee->count() > 0) {
 
-        $schedule->attendances()->create([
-            'check_in' => Date::excelToDateTimeObject($row['check_in']),
-            'check_out' => Date::excelToDateTimeObject($row['check_out']),
-            'employee_id' => $employee->id  
-        ]);
+            $schedule = Schedule::firstOrCreate([
+                'shift_id' => $shift->id,
+                'location_id' => $location->id,
+                'attendance_date' => Date::excelToDateTimeObject($row['attendance_date']),
+            ]);
+
+            $schedule->attendances()->create([
+                'check_in' => Date::excelToDateTimeObject($row['check_in']),
+                'check_out' => Date::excelToDateTimeObject($row['check_out']),
+                'employee_id' => $employee->id
+            ]);
+
+        }
 
     }
 
